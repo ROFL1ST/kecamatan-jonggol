124c0c6da586cfacb1d1ceeeb2976ff0d370df0a
C4ptainN3mo <endra1632@gmail.com>
2023-03-30 09:07:25 +0700

    P

--- src/pages/home/component/Galeri.jsx
+++ src/pages/home/component/Galeri.jsx
@@ -47,7 +47,11 @@ export default function Galeri() {
 
   return (
     <>
+<<<<<<< HEAD
       <div className="mt-28 mb-10 2xl:pl-16 lg:pl-10 lg:pt-20 lg:pb-10 flex flex-col items-center justify-center bg-[#3C903C]">
+=======
+      <div className="my-10 2xl:pl-16 lg:pl-10 lg:py-20 flex flex-col items-center justify-center bg-[#3C903C]">
+>>>>>>> 18854e17d4e35c75bb47712b098921b3e3c1ff7e
         <div className="w-full  flex lg:flex-row flex-col lg:justify-between justify-center gap-x-10 mt-20 mb-20 items-center">
           <div className="kiri lg:px-0 px-8 lg:w-1/3 flex flex-col lg:items-start items-center lg:mb-0 mb-10 lg:gap-y-0 gap-y-5">
             <h1 className="text-6xl text-white font-semibold">Foto</h1>
@@ -142,7 +146,7 @@ function CardFoto({ i }) {
             setOpen(true);
           }
         }}
-        className="lg:h-96 2xl:min-h-[30rem]  h-96 rounded-2xl w-full bg-cover bg-center shadow-2xl"
+        className="lg:h-96 2xl:min-h-[30rem] m-7 h-96 rounded-2xl w-full bg-cover bg-center shadow-2xl transition-all ease-in-out hover:scale-105 cursor-pointer"
         style={{
           backgroundImage: `url(${
             i.cover != null ? i.cover.thumbnail : NoImage
