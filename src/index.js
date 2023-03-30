18854e17d4e35c75bb47712b098921b3e3c1ff7e
Wakype <hilmim152bisnis@gmail.com>
2023-03-30 08:26:48 +0700

    update:  added agenda section & centering header

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import LogRocket from "logrocket";
-// LogRocket.init("kzccep/kecamatan");
+LogRocket.init("kzccep/kecamatan");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
