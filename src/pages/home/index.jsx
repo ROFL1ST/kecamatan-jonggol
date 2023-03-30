dc540fcf39bf04b0bde5de2a95399c863c2ff970
C4ptainN3mo <endra1632@gmail.com>
2023-03-30 09:29:18 +0700

    Update

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import Slider from './component/Slider';
-import Logoutama from '../../assets/logo/logo-utama.png';
-import siapmasjo from '../../assets/logo/siapmasjo.png';
-import sipahadasi from '../../assets/logo/sipahadesi.png';
-import sipaojol from '../../assets/logo/sipaojol.png';
-import Galeri from './component/Galeri';
-import { ArrowRight2, Calendar, Location } from 'iconsax-react';
-import CountUp from 'react-countup';
-import { getApi } from '../../API/restApi';
-import { useNavigate } from 'react-router-dom';
-import Lottie from 'lottie-react';
-import NotFound from '../../assets/json/93134-not-found.json';
-import ErrorIndicator from '../../assets/json/98642-error-404.json';
-import CustomButton from './component/customButton';
-import Agenda from '../agenda';
+import React from "react";
+import Slider from "./component/Slider";
+import Logoutama from "../../assets/logo/logo-utama.png";
+import siapmasjo from "../../assets/logo/siapmasjo.png";
+import sipahadasi from "../../assets/logo/sipahadesi.png";
+import sipaojol from "../../assets/logo/sipaojol.png";
+import Galeri from "./component/Galeri";
+import { ArrowRight2, Calendar, Location } from "iconsax-react";
+import CountUp from "react-countup";
+import { getApi } from "../../API/restApi";
+import { useNavigate } from "react-router-dom";
+import Lottie from "lottie-react";
+import NotFound from "../../assets/json/93134-not-found.json";
+import ErrorIndicator from "../../assets/json/98642-error-404.json";
+import CustomButton from "./component/customButton";
+import Agenda from "../agenda";
+import Potensi from "./component/Potensi";
 
 export default function Home() {
   const navigate = useNavigate();
@@ -21,7 +22,7 @@ export default function Home() {
   const [penduduk, setPenduduk] = React.useState();
   const getPenduduk = async () => {
     try {
-      await getApi('penduduk/total').then((res) => {
+      await getApi("penduduk/total").then((res) => {
         setPenduduk(res.data.data);
       });
     } catch (error) {
@@ -32,7 +33,7 @@ export default function Home() {
   const [asn, setAsn] = React.useState();
   const getAsn = async () => {
     try {
-      await getApi('pegawai/total').then((res) => {
+      await getApi("pegawai/total").then((res) => {
         setAsn(res.data.data);
       });
     } catch (error) {
@@ -43,7 +44,7 @@ export default function Home() {
 
   const getDesa = async () => {
     try {
-      await getApi('desa').then((res) => {
+      await getApi("desa").then((res) => {
         setDesa(res.data.data);
       });
     } catch (error) {
@@ -55,22 +56,22 @@ export default function Home() {
     {
       id: 1,
       count: penduduk,
-      title: 'Penduduk',
+      title: "Penduduk",
     },
     {
       id: 2,
       count: desa.length,
-      title: 'Jumlah Desa',
+      title: "Jumlah Desa",
     },
     {
       id: 3,
       count: asn,
-      title: 'Jumlah ASN',
+      title: "Jumlah ASN",
     },
     {
       id: 4,
       count: 158.9,
-      title: 'Luas Wilayah',
+      title: "Luas Wilayah",
     },
   ];
 
@@ -78,17 +79,17 @@ export default function Home() {
     {
       id: 1,
       url: siapmasjo,
-      desc: 'Lorem ipsum dolor sit amet consectetur. Eget laoreet donec commodo placerat viverra scelerisque ut. Sed lorem diam nunc cursus arcu nulla sapien. Non tempor donec suspendisse suspendisse egestas urna adipiscing feugiat. Sit velit eleifend eleifend felis arcu nisi. Eu gravida ultricies amet ut pretium purus aliquam porta a. Duis consectetur donec auctor lorem metus.',
+      desc: "Lorem ipsum dolor sit amet consectetur. Eget laoreet donec commodo placerat viverra scelerisque ut. Sed lorem diam nunc cursus arcu nulla sapien. Non tempor donec suspendisse suspendisse egestas urna adipiscing feugiat. Sit velit eleifend eleifend felis arcu nisi. Eu gravida ultricies amet ut pretium purus aliquam porta a. Duis consectetur donec auctor lorem metus.",
     },
     {
       id: 2,
       url: sipahadasi,
-      desc: 'Lorem ipsum dolor sit amet consectetur. Eget laoreet donec commodo placerat viverra scelerisque ut. Sed lorem diam nunc cursus arcu nulla sapien. Non tempor donec suspendisse suspendisse egestas urna adipiscing feugiat. Sit velit eleifend eleifend felis arcu nisi. Eu gravida ultricies amet ut pretium purus aliquam porta a. Duis consectetur donec auctor lorem metus.',
+      desc: "Lorem ipsum dolor sit amet consectetur. Eget laoreet donec commodo placerat viverra scelerisque ut. Sed lorem diam nunc cursus arcu nulla sapien. Non tempor donec suspendisse suspendisse egestas urna adipiscing feugiat. Sit velit eleifend eleifend felis arcu nisi. Eu gravida ultricies amet ut pretium purus aliquam porta a. Duis consectetur donec auctor lorem metus.",
     },
     {
       id: 3,
       url: sipaojol,
-      desc: 'Lorem ipsum dolor sit amet consectetur. Eget laoreet donec commodo placerat viverra scelerisque ut. Sed lorem diam nunc cursus arcu nulla sapien. Non tempor donec suspendisse suspendisse egestas urna adipiscing feugiat. Sit velit eleifend eleifend felis arcu nisi. Eu gravida ultricies amet ut pretium purus aliquam porta a. Duis consectetur donec auctor lorem metus.',
+      desc: "Lorem ipsum dolor sit amet consectetur. Eget laoreet donec commodo placerat viverra scelerisque ut. Sed lorem diam nunc cursus arcu nulla sapien. Non tempor donec suspendisse suspendisse egestas urna adipiscing feugiat. Sit velit eleifend eleifend felis arcu nisi. Eu gravida ultricies amet ut pretium purus aliquam porta a. Duis consectetur donec auctor lorem metus.",
     },
   ];
 
@@ -116,7 +117,7 @@ export default function Home() {
   const [loadBerita, setLoadBerita] = React.useState(true);
   const getBerita = async () => {
     try {
-      await getApi('berita').then((val) => {
+      await getApi("berita").then((val) => {
         // console.log(val.data.data);
         setBerita(val.data.data);
         setLoadBerita(false);
@@ -208,7 +209,6 @@ export default function Home() {
         {/* <Agenda />
          */}
 
-
         {/* program */}
         <div className="mt-28 mb-10 2xl:px-16 lg:px-10 px-8 flex flex-col items-center justify-center">
           <h1 className="text-4xl font-bold capitalize underline decoration-[#3C903C]">
@@ -222,13 +222,13 @@ export default function Home() {
             {/* box */}
           </div>
           <div
-            onClick={() => navigate('/aplikasi')}
+            onClick={() => navigate("/aplikasi")}
             onMouseEnter={handleMouseOver}
             onMouseLeave={handleMouseOut}
             className={`border-2 border-gray-400 rounded-full px-6 py-3 cursor-pointer ${
               hoverButton
-                ? 'bg-gray-400 text-white transition-all -translate-y-1 '
-                : 'text-gray-400 transition-all'
+                ? "bg-gray-400 text-white transition-all -translate-y-1 "
+                : "text-gray-400 transition-all"
             }`}
           >
             <p className="font-semibold ">Lebih Banyak</p>
@@ -247,12 +247,12 @@ export default function Home() {
             </h1>
             <div
               onClick={() => {
-                navigate('/berita');
+                navigate("/berita");
               }}
               className={`flex items-center justify-center gap-x-2  cursor-pointer ${
                 hoverButton2
-                  ? 'text-[#007100] transition-all -translate-x-1 -translate-y-1'
-                  : ' text-[#6B7280] transition-all'
+                  ? "text-[#007100] transition-all -translate-x-1 -translate-y-1"
+                  : " text-[#6B7280] transition-all"
               }`}
               onMouseEnter={handleMouseOver2}
               onMouseLeave={handleMouseOut2}
@@ -260,7 +260,7 @@ export default function Home() {
               <h1 className="font-bold 2xl:text-xl lg:text-lg">More News</h1>
               <ArrowRight2
                 size="22"
-                color={`${hoverButton2 ? '#547153' : '#6B7280'}`}
+                color={`${hoverButton2 ? "#547153" : "#6B7280"}`}
               />
             </div>
           </div>
@@ -275,10 +275,10 @@ export default function Home() {
           <div
             className={` content w-full 2xl:mt-20 mt-10   2xl:gap-x-16 lg:gap-x-4 lg:gap-y-0 gap-y-10 ${
               loadBerita
-                ? 'grid lg:grid-cols-4 grid-cols-1'
+                ? "grid lg:grid-cols-4 grid-cols-1"
                 : berita.length == 0
-                ? ''
-                : 'grid lg:grid-cols-4 grid-cols-1'
+                ? ""
+                : "grid lg:grid-cols-4 grid-cols-1"
             }`}
           >
             {loadBerita ? (
@@ -308,14 +308,14 @@ export default function Home() {
             <div className="lg:hidden flex justify-center items-center">
               <button
                 onClick={() => {
-                  navigate('/berita');
+                  navigate("/berita");
                 }}
                 onMouseEnter={handleMouseOver2}
                 onMouseLeave={handleMouseOut2}
                 className={` px-5 py-2 2xl:py-3 rounded-full lg:text-sm 2xl:text-base font-semibold ${
                   hoverButton2
-                    ? 'bg-[#007100] text-white transition-all border-2 border-[#007100]'
-                    : 'border-[#007100] border-2  text-[#007100] transition-all'
+                    ? "bg-[#007100] text-white transition-all border-2 border-[#007100]"
+                    : "border-[#007100] border-2  text-[#007100] transition-all"
                 }`}
               >
                 Selengkapnya
@@ -362,7 +362,7 @@ function CardInfo({ index, data }) {
         onMouseOut={handleMouseOut}
         className={`penduduk flex flex-col  items-center lg:gap-y-5 gap-y-1 lg:px-12 px-5  lg:py-16 py-10 transition-all cursor-default rounded-2xl border-2 ${
           isHovering &&
-          '-translate-y-1 -translate-x-1 shadow-xl transition-all bg-white border-0'
+          "-translate-y-1 -translate-x-1 shadow-xl transition-all bg-white border-0"
           // eslint-disable-next-line eqeqeq
         } `}
       >
@@ -373,7 +373,7 @@ function CardInfo({ index, data }) {
             decimal={data.count}
             end={data.count}
           />
-          {data.title == 'Luas Wilayah' && 'KM'}
+          {data.title == "Luas Wilayah" && "KM"}
         </div>
         <p className="lg:text-2xl text-lg">{data.title}</p>
       </div>
@@ -387,12 +387,12 @@ function CardInfoMobile({ data }) {
       <div
         className={`penduduk flex flex-col justify-center text-center  items-center  gap-y-1  px-5 border-[0.5px]  py-5 ${
           data.id == 1
-            ? 'rounded-tl-xl'
+            ? "rounded-tl-xl"
             : data.id == 2
-            ? 'rounded-tr-xl'
+            ? "rounded-tr-xl"
             : data.id == 3
-            ? 'rounded-bl-xl'
-            : 'rounded-br-xl'
+            ? "rounded-bl-xl"
+            : "rounded-br-xl"
         }`}
       >
         <div className="flex gap-x-3">
@@ -402,7 +402,7 @@ function CardInfoMobile({ data }) {
             decimal={data.count}
             end={data.count}
           />
-          {data.title == 'Luas Wilayah' && 'KM'}
+          {data.title == "Luas Wilayah" && "KM"}
         </div>
         <p className="text-xl ">{data.title}</p>
       </div>
@@ -441,8 +441,8 @@ function CardApp({ data }) {
               onMouseLeave={handleMouseOut}
               className={`cursor-pointer ${
                 isHovering
-                  ? 'text-[#3C903C] -translate-y-0.5 transition ease-in-out'
-                  : 'text-white transition ease-in-out'
+                  ? "text-[#3C903C] -translate-y-0.5 transition ease-in-out"
+                  : "text-white transition ease-in-out"
               }`}
             >
               Selengkapnya...
@@ -458,18 +458,18 @@ function CardBerita({ i }) {
   const navigate = useNavigate();
   const date = new Date(i.createdAt);
   var months = [
-    'Januari',
-    'Februari',
-    'Maret',
-    'April',
-    'May',
-    'Juni',
-    'Juli',
-    'Agustus',
-    'September',
-    'Oktober',
-    'November',
-    'Desember',
+    "Januari",
+    "Februari",
+    "Maret",
+    "April",
+    "May",
+    "Juni",
+    "Juli",
+    "Agustus",
+    "September",
+    "Oktober",
+    "November",
+    "Desember",
   ];
   var monthName = months[date.getMonth()];
 
@@ -504,118 +504,7 @@ function CardBerita({ i }) {
   );
 }
 
-function Potensi() {
-  const [potensi, setPotensi] = React.useState([]);
-  const [loadPotensi, setLoadPotensi] = React.useState(true);
-  const [potensiError, setPotensiError] = React.useState(false);
-  const [search, setSearch] = React.useState('');
-  const loader = [1, 2, 3, 4, 5, 6];
 
-  const getPotensi = async () => {
-    try {
-      await getApi(
-        `potensi-desa?limit=9&${search != '' && `key=${search}`}`
-      ).then((res) => {
-        setPotensi(res.data.data);
-        setLoadPotensi(false);
-      });
-    } catch (error) {
-      console.log(error);
-      setLoadPotensi(false);
-      setPotensiError(true);
-    }
-  };
-
-  React.useEffect(() => {
-    getPotensi();
-  }, [search]);
-  return (
-    <>
-      <div className="flex flex-col justify-center items-center py-20  mb-20 bg-[#3C903C] lg:px-20 px-8">
-        <div className="flex lg:flex-row flex-col lg:gap-y-0 gap-y-5 justify-between w-full items-end">
-          <div className="left title flex flex-col gap-y-5 lg:w-1/3 text-white">
-            <div className="h1">
-              <div className="  capitalize text-3xl font-bold">
-                Wisata Jonggol
-              </div>
-              <div className={`flex  my-2 h-0.5 w-44 bg-white`}></div>
-            </div>
-            <p>
-              Potensi desa Kecamatan Jonggol sangat besar, terutama dalam bidang
-              pertanian dan pariwisata. Desa-desa di Jonggol memiliki lahan yang
-              subur dan cocok untuk bercocok tanam, seperti padi, sayuran, dan
-              buah-buahan. Selain itu, desa-desa di Jonggol juga memiliki
-              keindahan alam yang memikat, seperti air terjun, danau, dan
-              bukit-bukit yang hijau, yang dapat menjadi destinasi wisata yang
-              menarik bagi wisatawan lokal maupun mancanegara.
-            </p>
-          </div>
-          <div className="right relative lg:w-auto w-full">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
-            <input
-              onChange={(e) => {
-                setSearch(e.target.value);
-              }}
-              type="text"
-              className="block w-full placeholder:text-gray-400 text-gray-400 pl-12 px-4 py-3 bg-white border rounded-xl focus:border-white focus:ring-white focus:outline-none focus:ring focus:ring-opacity-40"
-              placeholder="Search..."
-            />
-          </div>
-        </div>
-        <div
-          className={`mt-20  gap-10 w-full ${
-            loadPotensi
-              ? 'grid lg:grid-cols-3 grid-cols-1'
-              : potensi.length == 0 || potensiError
-              ? ''
-              : 'grid lg:grid-cols-3 grid-cols-1'
-          }`}
-        >
-          {!loadPotensi ? (
-            potensi.length != 0 ? (
-              potensi.map((i, key) => (
-                <>
-                  <CardPotensi key={key} data={i} />
-                </>
-              ))
-            ) : potensiError ? (
-              <>
-                <div className="flex flex-col justify-center items-center">
-                  <Lottie animationData={ErrorIndicator} />
-                  <h1 className="font-bold">Terjadi Kesalahan</h1>
-                </div>
-              </>
-            ) : (
-              <>
-                <div className="flex flex-col justify-center items-center">
-                  <Lottie animationData={NotFound} />
-                  <h1 className="font-bold text-white">
-                    Potensi Tidak Tersedia
-                  </h1>
-                </div>
-              </>
-            )
-          ) : (
-            loader.map((i, key) => <CardPotensiLoading />)
-          )}
-        </div>
-      </div>
-    </>
-  );
-}
 
 function CardPotensi({ data }) {
   return (
@@ -666,7 +555,7 @@ function CardAgenda({ dariJam, sampaiJam, lokasi, tanggal, agenda, key }) {
       </div>
 
       <div>
-        <CustomButton label={'detail'} stylingButton={''} />
+        <CustomButton label={"detail"} stylingButton={""} />
         <div>
           <Calendar size="32" color="#FF8A65" />
           <p>27 agustus 2023</p>
