57104bfe1e5e52dc55442eccf5e15ff2110bb844
C4ptainN3mo <endra1632@gmail.com>
2023-04-05 09:10:39 +0700

    P

---
+++ src/component/event.js
@@ -0,0 +1,37 @@
+export const myevents = [
+	{
+		id: 0,
+		title: "training",
+		start: new Date(2021, 5, 8, 9, 0, 0),
+		end: new Date(2021, 5, 8, 13, 0, 0),
+		resourceId: 1
+	},
+	{
+		id: 1,
+		title: "late lunch",
+		start: new Date(2021, 5, 8, 14, 0, 0),
+		end: new Date(2021, 5, 8, 16, 30, 0),
+		resourceId: 2
+	},
+	{
+		id: 2,
+		title: "fight",
+		start: new Date(2021, 5, 8, 8, 30, 0),
+		end: new Date(2021, 5, 8, 12, 30, 0),
+		resourceId: 3
+	},
+	{
+		id: 3,
+		title: "party",
+		start: new Date(2021, 5, 8, 7, 0, 0),
+		end: new Date(2021, 5, 8, 10, 30, 0),
+		resourceId: 4
+	}
+];
+
+export const myresources = [
+	{ id: 1, title: "spiderman" },
+	{ id: 2, title: "batman" },
+	{ id: 3, title: "aquaman" },
+	{ id: 4, title: "microman" }
+];
